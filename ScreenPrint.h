eac3ff142767e09b318088f5a3925303b741162a
K022G0044@g.neec.ac.jp <K022G0044@g.neec.ac.jp>
2023-04-24 12:40:38 +0900

    プロジェクト ファイルを追加します。

---
+++ ScreenPrint.h
@@ -0,0 +1,26 @@
+#pragma once
+#include"Matrix4x4.h"
+#include"Vector3.h"
+#include<Novice.h>
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+void MatrixScreenPrintf(int x, int y, Matrix4x4 matrix, const char* label) {
+	Novice::ScreenPrintf(x, y, "%s", label);
+	for (int row = 0; row < 4; ++row) {
+		for (int colum = 0; colum < 4; ++colum) {
+			Novice::ScreenPrintf(
+				x + colum * kColumnWidth, 20 + y + row * kRowHeight, "%6.02f",
+				matrix.m[row][colum]
+			);
+		}
+	}
+}
+
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
---
+++ Transfer.h
@@ -0,0 +1,7 @@
+#pragma once
+
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+
+Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
